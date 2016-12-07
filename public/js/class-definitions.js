d6df3491e329cfbba3dcc095809567c07b2b568b
Danika Harada <danikaharada@Danikas-MacBook-Air.local>
2016-12-06 21:28:29 -1000

    rainbow

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -182,6 +182,21 @@ var domains = {
  */
 
 
+ var rainbow = {
+
+ colors: {
+  red: "#F0280A",
+  orange: "#FF8800",
+  yellow: "#FFDD00",
+  green: "#51AB0C",
+  blue: "#1593ED",
+  indigo: "#5215ED",
+  violet: "#A915ED" },
+
+ isDouble: true
+ };
+
+
 /* Step 14
  *
  * Declare an array named "linuxFlavors"
